440b8b4a9b2447916b3a707c22b2cb1ce1cb442c
Paul Nikiforoff <velfor@tut.by>
2022-12-14 18:54:58 +0300

    Add files via upload

--- SFML_Lesson3/main.cpp
+++ SFML_Lesson3/main.cpp
@@ -2,6 +2,12 @@
 #include "settings.h"
 
 using namespace sf;
+bool pointInRect(RectangleShape bat, Vector2f point) {
+	return	bat.getPosition().x <= point.x &&
+		point.x <= bat.getPosition().x + BAT_SIZE.x &&
+		bat.getPosition().y <= point.y &&
+		point.y <= bat.getPosition().y + BAT_SIZE.y;
+}
 
 int main()
 {
@@ -85,12 +91,93 @@ int main()
 		else if (Keyboard::isKeyPressed(Keyboard::S)) {
 			leftBat.move(0.f, BAT_DY);
 		}
+		//не даем левой ракетке выйти за верхний край экрана
+		if (leftBat.getPosition().y < 0) {
+			leftBat.setPosition(BAT_OFFSET, 0.f);
+		}
+		//не даем левой ракетке выйти за нижний край экрана
+		if (leftBat.getPosition().y > WINDOW_HEIGHT - BAT_SIZE.y) {
+			leftBat.setPosition(BAT_OFFSET, WINDOW_HEIGHT - BAT_SIZE.y);
+		}
 		//ПРАВАЯ РАКЕТКА
 		if (Keyboard::isKeyPressed(Keyboard::Up)) {
 			rightBat.move(0.f, -BAT_DY);
 		}
 		else if (Keyboard::isKeyPressed(Keyboard::Down)) {
 			rightBat.move(0.f, BAT_DY);
+		}
+		//не даем правой ракетке выйти за верхний край экрана
+		if (rightBat.getPosition().y < 0) {
+			rightBat.setPosition(WINDOW_WIDTH - BAT_OFFSET - BAT_SIZE.x, 0.f);
+		}
+		//не даем правой ракетке выйти за нижний  край экрана
+		if (rightBat.getPosition().y < 0) {
+			rightBat.setPosition(WINDOW_WIDTH - BAT_OFFSET - BAT_SIZE.x, 
+				WINDOW_HEIGHT - BAT_SIZE.y);
+		}
+		//ОТСКОК МЯЧА ОТ РАКЕТОК
+		Vector2f leftTop{ ball.getPosition().x, ball.getPosition().y };
+		Vector2f midLeft{ ball.getPosition().x, ball.getPosition().y + BALL_RADIUS };
+		Vector2f leftBottom{ ball.getPosition().x, ball.getPosition().y +
+			2 * BALL_RADIUS };
+		Vector2f midTop{ ball.getPosition().x + BALL_RADIUS,  ball.getPosition().y };
+		Vector2f midBottom{ ball.getPosition().x + BALL_RADIUS,  ball.getPosition().y +
+			2 * BALL_RADIUS };
+		Vector2f rightTop{ ball.getPosition().x + 2 * BALL_RADIUS, 
+			ball.getPosition().y };
+		Vector2f midRight{ ball.getPosition().x + 2 * BALL_RADIUS, 
+			ball.getPosition().y + BALL_RADIUS };
+		Vector2f rightBottom{ ball.getPosition().x + 2 * BALL_RADIUS, 
+			ball.getPosition().y + 2 * BALL_RADIUS };
+		//ЛЕВАЯ РАКЕТКА
+		//от правого края левой ракетки
+		if (pointInRect(leftBat, midLeft) && pointInRect(leftBat, leftTop) ||
+			pointInRect(leftBat, midLeft) && pointInRect(leftBat, leftBottom))
+		{
+			ballSpeedX = -ballSpeedX;
+		}
+		//от верхнего края левой ракетки
+		if (pointInRect(leftBat, midBottom) && pointInRect(leftBat, leftBottom) ||
+			pointInRect(leftBat, midBottom) && pointInRect(leftBat, rightBottom))
+		{
+			ballSpeedY = -ballSpeedY;
+		}
+		//от нижнего края левого ракетки
+		if (pointInRect(leftBat, midTop) && pointInRect(leftBat, leftTop) ||
+			pointInRect(leftBat, midTop) && pointInRect(leftBat, rightTop))
+		{
+			ballSpeedY = -ballSpeedY;
+		}
+		//в ракетку попал только левый нижний угол
+		//в ракетку попал только левый верхний угол
+		if (pointInRect(leftBat, leftTop) || pointInRect(leftBat, leftBottom)) 
+		{
+			ballSpeedY = -ballSpeedY;
+		}
+		//ПРАВАЯ РАКЕТКА
+		//от левого края ПРАВОЙ ракетки
+		if (pointInRect(rightBat, midRight) && pointInRect(rightBat, rightTop) ||
+			pointInRect(rightBat, midRight) && pointInRect(rightBat, rightBottom))
+		{
+			ballSpeedX = -ballSpeedX;
+		}
+		//от верхнего края правой ракетки
+		if (pointInRect(rightBat, midBottom) && pointInRect(rightBat, leftBottom) ||
+			pointInRect(rightBat, midBottom) && pointInRect(rightBat, rightBottom))
+		{
+			ballSpeedY = -ballSpeedY;
+		}
+		//от нижнего края правой ракетки
+		if (pointInRect(rightBat, midTop) && pointInRect(rightBat, leftTop) ||
+			pointInRect(rightBat, midTop) && pointInRect(rightBat, rightTop))
+		{
+			ballSpeedY = -ballSpeedY;
+		}
+		//в ракетку попал только правый нижний угол
+		//в ракетку попал только правый верхний угол
+		if (pointInRect(rightBat, rightTop) || pointInRect(rightBat, rightBottom))
+		{
+			ballSpeedY = -ballSpeedY;
 		}
 		//3 отрисовка объектов и обновление окна
 		window.clear();
